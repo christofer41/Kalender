7528ef91def4f1776c920197725060ac142d2ea7
Christofer Carlsson <christofer.carlsson@medieinstitutet.se>
2019-11-28 14:52:44 +0100

    We added a way to remove the ToDo

--- ToDo/ToDo.js
+++ ToDo/ToDo.js
@@ -5,21 +5,38 @@ window.addEventListener("load", loadPage())
  */
 function loadPage(){    
     let addTodoButton = document.getElementById("addTodo");
+    let removeTodoButton = document.getElementById("removeTodo")
     toDoHolder = document.getElementById("toDoHolder");
     textPromptDiv = document.getElementById("textPromptDiv");
-    promptButton = document.getElementById("enterTheTextButton");
+    promptButton = document.getElementsByClassName("textButton");
+
     textPrompt = document.getElementById("textPrompt");
     toDoText = document.getElementById("toDoText");
 
+    promptDivAdd = document.getElementById("addPromptDiv");
+    promptDivRemove = document.getElementById("removePromptDiv");
+
     addTodoButton.addEventListener("click", addTodoPrompt);
+    removeTodoButton.addEventListener("click", removeTodoPrompt);
 }
 
 /**
  * When the user clicks the add button
  */
 function addTodoPrompt(){
+
     textPromptDiv.style.display = "block";
-    promptButton.addEventListener("click", addTodo)
+    promptDivAdd.style.display = "block";
+    promptDivRemove.style.display = "none";
+    promptButton[0].addEventListener("click", addTodo)
+}
+
+function removeTodoPrompt(){
+
+    textPromptDiv.style.display = "block";
+    promptDivAdd.style.display = "none";
+    promptDivRemove.style.display = "block"
+    promptButton[1].addEventListener("click", removeTodo)
 }
 
 /**
@@ -31,4 +48,9 @@ function addTodo(){
     textPromptDiv.style.display = "none";
 
     textPrompt.value = "";
+}
+
+function removeTodo(){
+    textPromptDiv.style.display = "none";
+    toDoText.innerHTML = "";
 }
