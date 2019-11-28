46941754308eea93bfc2e68a0de09bad21eef058
Embla <emblajonsson@gmail.com>
2019-11-28 17:59:35 +0100

    function for months added, header h1 font family added

--- layout/layout.js
+++ layout/layout.js
@@ -19,3 +19,44 @@ switch (month) {
         break;
 
 }
+
+switch (month) {
+    case 11:
+        document.getElementById('november').style.display = 'block'
+        break;
+
+    case 12:
+        document.getElementById('december').style.display = 'block'
+        break;
+
+    case 1:
+        document.getElementById('january').style.display = 'block'
+        break;
+    case 2:
+        document.getElementById('february').style.display = 'block'
+        break;
+    case 3:
+        document.getElementById('march').style.display = 'block'
+        break;
+    case 4:
+        document.getElementById('april').style.display = 'block'
+        break;
+    case 5:
+        document.getElementById('may').style.display = 'block'
+        break;
+    case 6:
+        document.getElementById('june').style.display = 'block'
+        break;
+    case 7:
+        document.getElementById('july').style.display = 'block'
+        break;
+    case 8:
+        document.getElementById('august').style.display = 'block'
+        break;
+    case 9:
+        document.getElementById('september').style.display = 'block'
+        break;
+    case 10:
+        document.getElementById('october').style.display = 'block'
+        break;
+}
