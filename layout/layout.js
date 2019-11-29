be1f0ff7ee68bf897720e5918e32f49a6c452143
Embla <emblajonsson@gmail.com>
2019-11-29 11:52:05 +0100

    added functions to js to make it look better, also added a loop to make the imgs disappear when not used

--- layout/layout.js
+++ layout/layout.js
@@ -1,62 +1,105 @@
+window.addEventListener('load', loadmonthImgs)
+window.addEventListener('load', loadSeasonImgs)
+
 const date = new Date()
 const month = date.getMonth() + 1
+let monthImgs = document.getElementsByClassName('monthImgs')
+let seasonImgs = document.getElementsByClassName('seasonImgs')
 
-switch (month) {
-    case 12 | 1 | 2:
-        document.getElementById('winter').style.display = 'block'
-        break;
+function makeSeasonImgInvisible() {
 
-    case 3 | 4 | 5:
-        document.getElementById('spring').style.display = 'block'
-        break;
+    for (let i = 0; i < seasonImgs.length; i++) {
+        seasonImgs[i].style.display = 'none'
 
-    case 6 | 7 | 8:
-        document.getElementById('summer').style.display = 'block'
-        break;
-
-    case 9 | 10 | 11:
-        document.getElementById('fall').style.display = 'block'
-        break;
-
+    }
 }
 
-switch (month) {
-    case 11:
-        document.getElementById('november').style.display = 'block'
-        break;
+function loadSeasonImgs() {
 
-    case 12:
-        document.getElementById('december').style.display = 'block'
-        break;
+    switch (month) {
+        case 12 | 1 | 2:
+            makeSeasonImgInvisible()
+            document.getElementById('winter').style.display = 'block'
+            break;
 
-    case 1:
-        document.getElementById('january').style.display = 'block'
-        break;
-    case 2:
-        document.getElementById('february').style.display = 'block'
-        break;
-    case 3:
-        document.getElementById('march').style.display = 'block'
-        break;
-    case 4:
-        document.getElementById('april').style.display = 'block'
-        break;
-    case 5:
-        document.getElementById('may').style.display = 'block'
-        break;
-    case 6:
-        document.getElementById('june').style.display = 'block'
-        break;
-    case 7:
-        document.getElementById('july').style.display = 'block'
-        break;
-    case 8:
-        document.getElementById('august').style.display = 'block'
-        break;
-    case 9:
-        document.getElementById('september').style.display = 'block'
-        break;
-    case 10:
-        document.getElementById('october').style.display = 'block'
-        break;
+        case 3 | 4 | 5:
+            makeSeasonImgInvisible()
+            document.getElementById('spring').style.display = 'block'
+            break;
+
+        case 6 | 7 | 8:
+            makeSeasonImgInvisible()
+            document.getElementById('summer').style.display = 'block'
+            break;
+
+        case 9 | 10 | 11:
+            makeSeasonImgInvisible()
+            document.getElementById('fall').style.display = 'block'
+            break;
+
+    }
+}
+
+function makeImgsInvisible() {
+
+    for (let i = 0; i < monthImgs.length; i++) {
+        monthImgs[i].style.display = 'none'
+
+    }
+}
+
+function loadmonthImgs() {
+    switch (month) {
+
+        case 1:
+            makeImgsInvisible()
+            document.getElementById('january').style.display = 'block'
+            break;
+        case 2:
+            makeImgsInvisible()
+
+            document.getElementById('february').style.display = 'block'
+            break;
+        case 3:
+            makeImgsInvisible()
+
+            document.getElementById('march').style.display = 'block'
+            break;
+        case 4:
+            makeImgsInvisible()
+            document.getElementById('april').style.display = 'block'
+            break;
+        case 5:
+            makeImgsInvisible()
+            document.getElementById('may').style.display = 'block'
+            break;
+        case 6:
+            makeImgsInvisible()
+            document.getElementById('june').style.display = 'block'
+            break;
+        case 7:
+            makeImgsInvisible()
+            document.getElementById('july').style.display = 'block'
+            break;
+        case 8:
+            makeImgsInvisible()
+            document.getElementById('august').style.display = 'block'
+            break;
+        case 9:
+            makeImgsInvisible()
+            document.getElementById('september').style.display = 'block'
+            break;
+        case 10:
+            makeImgsInvisible()
+            document.getElementById('october').style.display = 'block'
+            break;
+        case 11:
+            makeImgsInvisible()
+            document.getElementById('november').style.display = 'block'
+            break;
+        case 12:
+            makeImgsInvisible()
+            document.getElementById('december').style.display = 'block'
+            break;
+    }
 }
